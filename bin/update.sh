11200a0f71606dc02f77d38a5c8ef833bdc29b8c
Mark A. Jensen <maj-git@fortinbras.us>
2023-01-02 09:09:38 -0500

    same

--- bin/update.sh
+++ bin/update.sh
@@ -4,8 +4,7 @@ curl http://$NEO_URL:7687
 if (( $? == 52 ))
 then
      update-ghquery.pl | load-update.pl | neo4j-client --insecure $NEO_URL 7687
-     logger -p syslog.info -f minisrv.log
 else
-  logger -p syslog.err "[ERROR] Neo4j server is not responding - update aborted"
+  1>&2 echo "[ERROR] Neo4j server is not responding - update aborted" 
 fi
 
