6a2f0d54c51cfe3fbae1dda0d9a0a2939e2bf543
Mark A. Jensen <maj-git@fortinbras.us>
2023-01-01 23:32:30 -0500

    update.sh : push logs to syslog
    also check neo4j server and abort if not available

--- bin/update.sh
+++ bin/update.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-update-ghquery.pl | load-update.pl | neo4j-client --insecure $NEO_URL 7687
+# update jossgraph, push log into syslog
+curl http://$NEO_URL:7687
+if (( $? == 52 ))
+then
+     update-ghquery.pl | load-update.pl | neo4j-client --insecure $NEO_URL 7687
+     logger -p jossgraph.info -f minisrv.log
+else
+  logger -p jossgraph.error "[ERROR] Neo4j server is not responding - update aborted"
+fi
+
