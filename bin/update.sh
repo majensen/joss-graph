04a20b930b3afa4a228fb88f67ee948132b733ab
Mark A. Jensen <maj-git@fortinbras.us>
2023-01-02 09:18:10 -0500

    same

--- bin/update.sh
+++ bin/update.sh
@@ -5,7 +5,7 @@ if (( $? == 52 ))
 then
      update-ghquery.pl | load-update.pl | neo4j-client --insecure $NEO_URL 7687
 else
-  1>&2 echo "[ERROR] Neo4j server is not responding - update aborted" |
+  echo "[ERROR] Neo4j server is not responding - update aborted" |
     tee >> ${JGLOG:-jossgraph.log}
 fi
 
